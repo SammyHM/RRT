0bd056eec811d264b0d73d374d797896927f3584
Samuel Hidalgo <samuelluishm@gmail.com>
2024-02-24 19:20:15 +0100

    RTTv1.0.7 Fixed building application in virtual environement.

--- src/build.py
+++ src/build.py
@@ -4,7 +4,8 @@ from sys import executable
 if __name__ == "__main__":
     """ Creates a python environment with the applications requirements. 
     """
+    env_executable = ".venv/Scripts/python.exe"
     run([executable, "-m", "venv", ".venv"])
-    run([executable, "-m", "pip", "install", "-r", "requirements.txt"])
-    run([executable, "-m", "nltk.downloader", "stopwords"])
-    run([executable, "-m", "nltk.downloader", "punkt"])
+    run([env_executable, "-m", "pip", "install", "-r", "requirements.txt"])
+    run([env_executable, "-m", "nltk.downloader", "stopwords"])
+    run([env_executable, "-m", "nltk.downloader", "punkt"])
