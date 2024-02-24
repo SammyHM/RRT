084be018f1f933c1c502afd10fa6f6fd1dc18415
Samuel Hidalgo <samuelluishm@gmail.com>
2024-02-24 14:06:56 +0100

    RRTv1.0.6 Fixed build.py unable to recognize paths.

--- src/build.py
+++ src/build.py
@@ -4,7 +4,7 @@ from sys import executable
 if __name__ == "__main__":
     """ Creates a python environment with the applications requirements. 
     """
-    run([executable, "venv", ".venv"])
-    run([executable, "pip", "install", "requirements.txt"])
-    run([executable, "nltk.downloader", "stopwords"])
-    run([executable, "nltk.downloader", "punkt"])
+    run([executable, "-m", "venv", ".venv"])
+    run([executable, "-m", "pip", "install", "-r", "requirements.txt"])
+    run([executable, "-m", "nltk.downloader", "stopwords"])
+    run([executable, "-m", "nltk.downloader", "punkt"])
